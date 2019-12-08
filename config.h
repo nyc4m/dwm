b4c347596ab8e8e0fb3702fe9443d25091a4c3b1
Baptiste Prunot <baptiste.prunot@protonmail.com>
2019-12-08 17:40:56 +0100

    Set font

--- config.h
+++ config.h
@@ -1,12 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "Hack Nerd Font:size=12" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#11405C";
 static const char col_gray2[]       = "#70C2B4";
