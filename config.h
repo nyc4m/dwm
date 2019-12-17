e14fc23dd848bc70a9e167baf9a212c33b72cbfc
nyc4m <baptiste.prunot@utt.fr>
2019-12-17 17:09:36 +0100

    Reset english keys

--- config.h
+++ config.h
@@ -97,16 +97,16 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
-    TAGKEYS(                        XK_ampersand,                      0)
-    TAGKEYS(                        XK_eacute,                         1)
-    TAGKEYS(                        XK_quotedbl,                       2)
-    TAGKEYS(                        XK_apostrophe,                     3)
-    TAGKEYS(                        XK_parenleft,                      4)
-    TAGKEYS(                        XK_minus,                          5)
-    TAGKEYS(                        XK_egrave,                         6)
-    TAGKEYS(                        XK_underscore,                     7)
-    TAGKEYS(                        XK_ccedilla,                       8)
-    TAGKEYS(                        0xe0,                              9)
+    TAGKEYS(                        XK_1,                               0)
+    TAGKEYS(                        XK_2,                               1)
+    TAGKEYS(                        XK_3,                               2)
+    TAGKEYS(                        XK_4,                               3)
+    TAGKEYS(                        XK_5,                               4)
+    TAGKEYS(                        XK_6,                               5)
+    TAGKEYS(                        XK_7,                               6)
+    TAGKEYS(                        XK_8,                               7)
+    TAGKEYS(                        XK_9,                               8)
+    TAGKEYS(                        0xe0,                               9)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
